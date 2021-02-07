e4df2e5e40c6a76b5ab2ded6ab9175062f2629ee
PY <pyhuang97@gmail.com>
2021-02-07 12:14:50 +0800

    extracting part of data.

--- main.py
+++ main.py
@@ -4,4 +4,6 @@ from utils import *
 data = Data(base_dir="data")
 wikipages = data.get_wikipages()
 trainjsonl = data.get_trainjsonl()
+
+
 # %%
--- utils/data.py
+++ utils/data.py
@@ -1,7 +1,9 @@
 #%%
 import json
+import random
 from pathlib import Path
 from unicodedata import normalize
+import argparse
 
 
 class Data:
@@ -10,37 +12,58 @@ class Data:
         self.trainjsonl_pth = Path(base_dir) / "train.jsonl"
         self.shared_task_dev_pth = Path(base_dir) / "train_task_dev.jsonl"
         self.shared_task_test_pth = Path(base_dir) / "train_task_test.jsonl"
+        self.train_ids = None
+        self.wiki_ids = None
         self.wikipages = None
         self.trainjsonl = None
+
+    def get_sub_data(self, train_ids):
+        self.train_ids = train_ids
+        self.wiki_ids = self.get_wiki_ids(train_ids)
 
     def get_wikipages(self):
         if self.wikipages is None:
             self.wikipages = self.load_wikipages()
         return self.wikipages.copy()
 
-    def get_trainjsonl(self):
+    def get_processed_trainjsonl(self):
         if self.trainjsonl is None:
             wikipages = self.get_wikipages()
-            self.trainjsonl = self.load_trainjsonl(wikipages)
+            self.trainjsonl = self.load_processed_trainjsonl(wikipages)
         return self.trainjsonl
+
+    def get_trainjsonl(self):
+        return parse_jsonl(self.trainjsonl_pth)
 
     #%% load wiki-pages
     def load_wikipages(self):
         data = {}
         fnames = list(self.wikipages_dir.glob("wiki-*.jsonl"))
+        print("--- Load Wiki-Pages ---")
         for step, fname in enumerate(fnames, 1):
             print(f"[{step}/{len(fnames)}] Processing {fname}")
             wiki_data = parse_jsonl(fname)
             wiki_data_dict = to_dict(wiki_data)
-            for dic in wiki_data_dict.values():
-                dic["lines"] = dic["lines"].split("\n")
-            data.update(wiki_data_dict)
+            if self.wiki_ids is not None:
+                for wiki_id in self.wiki_ids:
+                    if wiki_id in wiki_data_dict:
+                        dic = wiki_data_dict[wiki_id]
+                        dic["lines"] = dic["lines"].split("\n")
+                        data.update({wiki_id: dic})
+            else:
+                for dic in wiki_data_dict.values():
+                    dic["lines"] = dic["lines"].split("\n")
+                data.update(wiki_data_dict)
         return data
 
     #%% load train_jsonl
-    def load_trainjsonl(self, wikipages):
+    def load_processed_trainjsonl(self, wikipages):
         json_list = parse_jsonl(self.trainjsonl_pth)
+        out = []
+        print("--- Processed train.jsonl ---")
         for dic in json_list:
+            if self.train_ids and dic["id"] not in self.train_ids:
+                continue
             processed_evidence_sets = []
             for evidence_sets in dic["evidence"]:
                 evidence_sentences = []
@@ -53,7 +76,35 @@ class Data:
                     evidence_sentences.append(evidence_sentence)
                 processed_evidence_sets.append(evidence_sentences)
             dic["evidence_sentences"] = processed_evidence_sets
-        return json_list
+            out.append(dic)
+        return out
+
+    #%% load train_jsonl
+    def get_wiki_ids(self, train_ids: list):
+        wiki_ids = set()
+        trainjsonl = self.get_trainjsonl()
+        for dic in trainjsonl:
+            if dic["id"] not in train_ids:
+                continue
+            for evidence_sets in dic["evidence"]:
+                for evidence in evidence_sets:
+                    if evidence[2] is None or evidence[3] is None:
+                        continue
+                    evidence_id = normalize("NFKD", evidence[2])
+                    wiki_ids.add(evidence_id)
+        return list(wiki_ids)
+
+
+class SubData(Data):
+    def __init__(self, base_dir="data", ratio=0.1, seed=1009):
+        super().__init__(base_dir=base_dir)
+        trainjsonl = parse_jsonl(Path(base_dir) / "train.jsonl")
+        random.seed(seed)
+        random.shuffle(trainjsonl)
+        train_ids = [
+            trainjson["id"] for trainjson in trainjsonl[: int(len(trainjsonl) * ratio)]
+        ]
+        self.get_sub_data(train_ids)
 
 
 #%% parse jsonl format data
@@ -78,3 +129,39 @@ def to_dict(list_of_dict):
         output[_id] = dic
 
     return output
+
+
+#%% test
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Processed Data.")
+    parser.add_argument(
+        "--infolder", type=str, default="data", help="folder that store the data."
+    )
+    parser.add_argument(
+        "--outfolder",
+        type=str,
+        default="processed_data",
+        help="folder to save the processed data.",
+    )
+    parser.add_argument(
+        "--ratio", type=float, default=0.1, help="Ratio of data to get.",
+    )
+    args = parser.parse_args()
+
+    if args.ratio == 1:
+        data = Data(args.infolder)
+    else:
+        data = SubData(base_dir=args.infolder, ratio=args.ratio)
+
+    wikidata = data.get_wikipages()
+    trainjsonl = data.get_processed_trainjsonl()
+
+    Path(args.outfolder).mkdir(parents=True, exist_ok=True)
+    with open(
+        Path(args.outfolder) / "wikipages.json", "w", encoding="utf-8"
+    ) as out_file:
+        json.dump(wikidata, out_file, ensure_ascii=False, indent=4)
+
+    with open(Path(args.outfolder) / "train.json", "w", encoding="utf-8") as out_file:
+        json.dump(trainjsonl, out_file, ensure_ascii=False, indent=4)
+# %%
